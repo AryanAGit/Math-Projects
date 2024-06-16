464008a9bf1df657c9c5781ebc23b71bf038d784
AryanAGit <aryana91006@gmail.com>
2024-06-16 14:08:04 -0700

    mandelbrotC++.cpp

--- mandelbrotC++.cpp
+++ mandelbrotC++.cpp
@@ -1,12 +1,9 @@
-// mandelbrotC++.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 #include <cmath>
 #include <fstream>
 #include <string>
 #include <chrono>
-#include <opencv.hpp>
+
 using namespace std;
 const int iterations = 6000;
 const int check = 500;
@@ -21,7 +18,7 @@ int mandelbrot(Complex c);
 void square(Complex& c);
 int main()
 {
-  cv::Mat image
+
   ofstream fout;
   ofstream fout2;
   ofstream fout3;
@@ -90,14 +87,3 @@ int mandelbrot(Complex c) {
   }
   return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
