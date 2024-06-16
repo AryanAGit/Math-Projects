7bf8379ddedf441688f547f78f4ac5eff99f5256
AryanAGit <aryana91006@gmail.com>
2024-06-16 14:23:48 -0700

    Update prime.cpp

--- prime.cpp
+++ prime.cpp
@@ -66,7 +66,7 @@ int findPrimes(int lim, int* listPrimes) {
 
 int main()
 {
-	cout << "Welcome. This program finds all prime numbers between 0 and a given integer inclusive. Enter an integer or enter 0 to quit. ";
+	cout << "Welcome. This program finds all prime numbers between 0 and a given integer inclusive. Enter an integer or enter 0 to quit. Currently goes up to 7919";
 	int limit;
 	int listPrimes[1000] = { 2, 3 };
 	int numOfPrimes = 2;
@@ -82,13 +82,3 @@ int main()
 
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
