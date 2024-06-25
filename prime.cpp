aac6ae6dc21ee0beec3ac147005115f7987f8b87
AryanAGit <aryana91006@gmail.com>
2024-06-25 14:56:32 -0700

    Update prime.cpp
    The program now is not restricted to a set number of primes it can store. I updated the program's implementation to use linked list, not array to store the primes, allowing for as many primes to be calculated as needed dynamically.

--- prime.cpp
+++ prime.cpp
@@ -2,83 +2,89 @@
 
 // Once you loop through the prime list for the integer, if it %!=0 for all primes, then its a prime
 
+struct node {
+	int num;
+	node* next;
+};
+
 #include <iostream>
 using namespace std;
-int findPrimes(int lim, int* listPrimes) {
-	
-	int primes = 2;
-	if (lim < 5 && lim>0) 
-	{
-		if (lim >= 3) 
-		{
-			cout << "There are two primes between 0 and " << lim << endl;
-			cout << "2 is a prime." << endl;
-			cout << "3 is a prime." << endl;
-		}
-		else if (lim >= 2) 
-		{
-			cout << "There is one prime between 0 and " << lim << endl;
-			cout << "2 is a prime." << endl;
-		}
-		else 
-		{
-			cout << "There are no primes between 0 and " << lim << endl;
-		}
-	}
-	else if (lim >= 5) 
-	{
-		for (int i = 5; i <= lim; i = i + 2) 
-		{
-			int primeCount = 0;
 
-			for (int j = 0; j < primes; j++) 
-			{
-				if (i % listPrimes[j] == 0 ) 
-				{
-					break;
-				}
-				else 
-				{
-					primeCount++;
-				}
-			}
-
-			if (primeCount == primes && primes < 1000) 
-			{
-				listPrimes[primes] = i;
-				primes++;
-			}
-		}
-		for (int i = 0; i < primes; i++) 
-		{
-			cout << listPrimes[i] << " is a prime." << endl;
-		}
-		cout << "There are " << primes << " prime numbers between 0 and " << lim << " inclusive. " << endl;
-		
-	}
-
-	else 
-	{
-		cout << "Invalid input entered. Please enter an integer value greater than 0." << endl;
-	}
-	return primes;
-}
+int findPrimes(int limit, node*& head, node*& tail, int numPrimes);
 
 int main()
 {
-	cout << "Welcome. This program finds all prime numbers between 0 and a given integer inclusive. Enter an integer or enter 0 to quit. Currently goes up to 7919";
+	cout << "Welcome. This program finds all prime numbers between 0 and a given integer inclusive. Enter an integer or enter 0 to quit. ";
 	int limit;
-	int listPrimes[1000] = { 2, 3 };
-	int numOfPrimes = 2;
+	node* head = new node{ 2, nullptr };
+	node* tail = new node{ 3, nullptr };
+	head->next = tail;
+	int numPrimes = 2;
 	cin >> limit;
 	cin.ignore(1000, 10);
 	while (limit != 0) {
 		
-		numOfPrimes =findPrimes(limit, listPrimes);
+		numPrimes = findPrimes(limit, head, tail, numPrimes);
 		cout << "Press 0 to quit, or enter another integer. " << endl;
 		cin >> limit;
 		cin.ignore(1000, 10);
 	}
-
+	cout << "Thanks, come again!";
 }
 
+int findPrimes(int limit, node*& head, node *& tail, int numPrimes) {
+	int primes = numPrimes;
+
+	if (limit < 5 && limit>0)
+	{
+		if (limit >= 3)
+		{
+			cout << "There are two primes between 0 and " << limit <<" inclusine" << endl;
+			cout << "2 is a prime." << endl;
+			cout << "3 is a prime." << endl;
+		}
+		else if (limit >= 2)
+		{
+			cout << "There is one prime between 0 and " << limit <<" inclusive"<<endl;
+			cout << "2 is a prime." << endl;
+		}
+		else
+		{
+			cout << "There are no primes between 0 and " << limit << " inclusive"<< endl;
+		}
+	}
+
+	else if (limit <= 0) {
+		cout << "Invalid input entered. Please enter an integer value greater than 0." << endl;
+
+	}
+
+	else {
+		for (int i = 5; i <= limit; i += 2) {
+			int count = 0;
+			node* it = head;
+			for (int j = 0; j < primes; j++) {
+				if (i % it->num == 0) break;
+
+				else {
+					count++;
+					it = it->next;
+				}
+			}
+			if (count == primes) {
+				node* newPrime = new node{ i, nullptr };
+				tail->next = newPrime;
+				tail = newPrime;
+				primes++;
+			}
+		}
+		node* it = head;
+		for (int i = 0; i < primes; i++) {
+			cout << it->num << " is a prime." << endl;
+			it = it->next;
+		}
+		cout << "There are " << primes << " prime numbers between 0 and " << limit << " inclusive." << endl;
+	}
+
+	return primes;
+}
